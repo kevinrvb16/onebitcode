0b816e2a0bcc6354de7fc59daa9a45e626058c17
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-12-21 21:28:11 -0300

    Finalizado

--- JavaScript/Arrays/sliceSpliceBidimensional/index.js
+++ JavaScript/Arrays/sliceSpliceBidimensional/index.js
@@ -4,12 +4,29 @@ const hitchedSpaceships = [
     ["Helmet", 5, false],
     ["Elemental", 3, true],
     ["Darwth", 15, false]
-  ];
+];
+let result = ''
 
-  const filteredSpaceships = hitchedSpaceships.filter((value) => {
+const filteredSpaceships = hitchedSpaceships.filter((value) => {
     return value[0].length >= 9
-  })
+})
 
-  filteredSpaceships.forEach((value)=> {
-      console.log(value[0])
-  })
+filteredSpaceships.forEach((value) => {
+    result +=`${value[0]}, `
+})
+result +='\n'
+
+const indexOfSpaceship = hitchedSpaceships.findIndex((value) => {
+    return value[2] === false
+})
+if (indexOfSpaceship) {
+    result += `${hitchedSpaceships[indexOfSpaceship][1]}, `
+}
+result += '\n'
+
+hitchedSpaceships.slice().forEach((value) => {result += `${value[0].toUpperCase()}, `})
+
+result += '\n'
+
+alert(result)
+console.log(result)
