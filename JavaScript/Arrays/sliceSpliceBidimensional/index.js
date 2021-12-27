876815c7fb0e0d63bc7856df9cab8088ad1cf017
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-12-27 15:57:33 -0300

    adicionado revisões sobre o exercicio de acordo com a revisão

--- JavaScript/Arrays/sliceSpliceBidimensional/index.js
+++ JavaScript/Arrays/sliceSpliceBidimensional/index.js
@@ -8,21 +8,21 @@ const hitchedSpaceships = [
 let result = ''
 
 const filteredSpaceships = hitchedSpaceships.filter((value) => {
-    return value[0].length >= 9
+    return value[1] > 9
 })
-
+result += 'Espaçonaves com mais de 9 tripulantes: '
 filteredSpaceships.forEach((value) => {
     result +=`${value[0]}, `
 })
-result +='\n'
+result +='\nPlataform com processo de engate: '
 
 const indexOfSpaceship = hitchedSpaceships.findIndex((value) => {
     return value[2] === false
 })
-if (indexOfSpaceship) {
-    result += `${hitchedSpaceships[indexOfSpaceship][1]}, `
-}
-result += '\n'
+
+result += `${indexOfSpaceship + 1}, `
+
+result += '\nEspaçonaves destacadas: '
 
 hitchedSpaceships.slice().forEach((value) => {result += `${value[0].toUpperCase()}, `})
 
