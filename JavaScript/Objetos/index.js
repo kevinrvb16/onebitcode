734a379f02d518a7c71fb3b536c86a322801e5f8
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2021-12-29 20:04:24 -0300

    Feitas correções de acordo com a resolução do professor que pareceu melhor

--- JavaScript/Objetos/index.js
+++ JavaScript/Objetos/index.js
@@ -1,18 +1,48 @@
-let spaceship = {}
-spaceship.name = prompt('Nome:')
-spaceship.type = prompt("Tipo: ")
-spaceship.maxVelocity = prompt('Velocidade máxima: ')
-let increment, velocity = 0
-let option
-
-function incrementFunction() {
-    velocity += increment
+let spaceship = {
+    velocity: 0,
+    incrementFunction: function (increment) {
+        this.velocity += increment
+    }
 }
 
-option = prompt('Quer acelerar ou parar a nave?')
-if (option == 'acelerar') {
-    increment = prompt('O quanto quer acelerar?')
-    incrementFunction()
+function registerSpaceship() {
+    spaceship.name = prompt('Informe o nome da nave')
+    spaceship.type = prompt("Informe o tipo da nave")
+    spaceship.maxVelocity = Number(prompt('Informe a velocidade máxima da nave(km/s)'))
 }
 
-alert(`Nave: ${spaceship.name}\nTipo: ${spaceship.type}\nVelocidade Máxima: ${spaceship.maxVelocity}\nVelocidade: ${velocity}`)
+function acelerate() {
+    let acceleration = Number(prompt('Quanto você quer acelerar? (km/s)'))
+    spaceship.incrementFunction(acceleration)
+    if (spaceship.velocity > spaceship.maxVelocity) {
+        alert("VELOCIDADE MÁXIMA ULTRAPASSADA\nVelocidadedaNave: " +
+            spaceship.velocity + "km/s\n" +
+            "velocidade máxima da Nave: " + spaceship.maxVelocity + "km/s")
+    }
+}
+
+function stop() {
+    alert("Nome: " + spaceship.name + "\nTipo: "  + spaceship.type + "\nVelocidade da nave: " +
+    spaceship.velocity + "\nMáxima da Nave: " + spaceship.maxVelocity)
+    spaceship.velocity = 0
+}
+
+function showMenu() {
+    let chosenOption
+    do {
+        chosenOption =prompt("Você deseja:\n1- Acelerar\n2- Parar")
+        switch(chosenOption) {
+            case "1":
+                acelerate()
+                break
+            case "2":
+                stop()
+                break
+            default:
+                alert("Opção inválida")
+        }
+    }while (chosenOption != "2")
+}
+
+registerSpaceship()
+showMenu()
