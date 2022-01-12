54ce4a11181f079fa8688686acb9fa7a350525cd
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-01-12 18:30:48 -0300

    Finalizado parte de JS para aula de Seletores Formularios

---
+++ JavaScript/SELETORES_FORMULARIOS/form-selector.js
@@ -0,0 +1,11 @@
+function showinfo() {
+    let name = document.querySelector("input[name='name']").value;
+    let favoriteColor = document.querySelector("select[name='color'] option:checked").textContent
+    let likeProgramming = document.querySelector("input[name='like-programming']:checked").value
+    let developerOptions = document.querySelectorAll("input[name='developer-options']:checked")
+    let optionsValue = []
+    developerOptions.forEach( element => { optionsValue.push( element.value)})
+    let optionsChecked = optionsValue.join(", ")
+    alert("Nome: " + name + "\nCor primária: " + favoriteColor + "\nGosta de programar? " + likeProgramming + "\n" +
+    "Conhecimentos em programação Web: " + optionsChecked)
+}
