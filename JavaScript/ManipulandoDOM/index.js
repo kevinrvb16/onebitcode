1c5d44425ee16462cb1e4b41942c2bbbba4ddaaa
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-01-11 21:48:27 -0300

    Lógica concluída com ecercício funcionando

--- JavaScript/ManipulandoDOM/index.js
+++ JavaScript/ManipulandoDOM/index.js
@@ -1,9 +1,19 @@
 function registerHouse() {
-    let bairro = document.getElementById('bairro').value
-    let n = document.getElementById('numero').value
-    let area = document.getElementById('area').value
-    let cidade = document.getElementById('cidade').value
-    let elementDIV = document.getElementById('list')
-    let conteudoNovo = document.createTextNode('Bairro:' + bairro + '\nÁrea:' + area + '\nCidade: ' + cidade + '\nNúmero: ' + n)
-    elementDIV.appendChild(conteudoNovo)
+    let bairro = document.getElementById('bairro').value;
+    let n = document.getElementById('numero').value;
+    let area = document.getElementById('area').value;
+    let cidade = document.getElementById('cidade').value;
+    let elementDIV = document.createElement('div');
+    document.body.appendChild(elementDIV);
+    let conteudoNovo = document.createTextNode('Bairro:' + bairro + '\nÁrea:' + area + '\nCidade: ' + cidade + '\nNúmero: ' + n);
+    let button = document.createElement('button');
+    button.textContent = 'Remover'
+    elementDIV.appendChild(conteudoNovo);
+    elementDIV.appendChild(button);
+    button.addEventListener('click',deleteHouse , false);
+}
+
+function deleteHouse(e) {
+    let deletedDIV = e.currentTarget.parentNode
+    deletedDIV.remove();
 }
