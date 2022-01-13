b5a67b4a70633cf946ca2a78a792d90d8a5777f1
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-01-13 20:17:12 -0300

    app.js ainda em construção

---
+++ JavaScript/ManipulandoDOM/ex_PRATICANDO_DOM/app.js
@@ -0,0 +1,35 @@
+class App{
+    cadProperty() {
+        event.preventDefault();
+        let property = document.querySelector('input[name=type]').value;
+        let area = document.querySelector('input[name=area]').value;
+        let isRent = document.querySelector('input[name=isRent]:checked').value;
+    
+        let ulElem = document.getElementById('propertyList')
+        let ilElem = document.createElement('li');
+        ulElem.appendChild(ilElem);
+        if (isRent == 'ALUGADO') {
+            ilElem.style.backgroundColor = 'red';
+            ilElem.style.color = 'white';
+            ilElem.style.width = '33%';
+            ilElem.innerText = isRent + '\n\nTipo de propriedade: ' + property + "\n Área: " +  area + "\nEstá alugado? "
+        } else {
+            ilElem.innerText = 'Tipo de propriedade: ' + property + "\n Área: " +  area + "\n" + isRent
+        }
+        let button = document.createElement('button');
+        ilElem.appendChild(button);
+        ilElem.style.lineHeight = '36px'
+        ilElem.style.padding = '16px'
+        ilElem.style.justifyContent = 'space-between'
+        button.setAttribute('onclick', 'removeProperty(this)')
+        button.innerText = "Remover"
+        button.style.width = "120px"
+        button.style.padding = "6px"
+        document.querySelector('form').reset();
+    }
+    
+    removeProperty(button) {
+        let propertyToRemove = button.parentNode
+        document.getElementById('propertyList').removeChild(propertyToRemove);
+    }
+}
