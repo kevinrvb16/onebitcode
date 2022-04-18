cb6295c11bc467a0e1e7bbe3d08efb7cb423a47a
Kevin Rafael <kevin.velez@ahgora.com.br>
2022-04-18 09:29:46 -0300

    utilizando then

---
+++ JavaScript/Promise/UtilizandoThen/src/index.js
@@ -0,0 +1,30 @@
+let komodoShip = {
+    name: "Komodo",
+    velocity: 80,
+    acceleration: 10
+}
+
+const velocityAfter2Seconds = (velocity, acceleration) => {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            if (acceleration > 0) {
+                velocity += acceleration * 2
+                console.log(`Nova velocidade: ${velocity}`)
+                resolve(velocity)
+            } else {
+                console.log("Aceleração inválida")
+                reject("Não possui aceleração")
+            }
+        }, 1000)
+    })
+}
+
+velocityAfter2Seconds(komodoShip.velocity, komodoShip.acceleration).then(velocity => {
+    komodoShip.velocity = velocity
+    console.log("Depois de acelerar:\n", komodoShip)
+}).catch(message => {
+    console.log(`Komodo: ${message}`)
+})
+
+console.log("Execução de Promises")
+console.log(komodoShip)
