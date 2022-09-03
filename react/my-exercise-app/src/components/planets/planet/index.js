85f0701e073737fd31cf69228ec03210cb792f5e
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-09-03 20:05:48 -0300

    Exercício de react hooks concluído

--- react/my-exercise-app/src/components/planets/planet/index.js
+++ react/my-exercise-app/src/components/planets/planet/index.js
@@ -1,45 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DescriptionWithLink from "../../shared/descriptionWithLink";
 import GrayImg from "../../shared/img";
 
 const getSatellites = async (id_da_lua) => {
-    let response = await fetch(`http://localhost:3000/api/${id_da_lua}.json`);
-    let data = await response.json();
-    return data;
-}
-class Planet extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            satellites: [],
-        }
-    }
+  let response = await fetch(`http://localhost:3000/api/${id_da_lua}.json`);
+  let data = await response.json();
+  return data;
+};
 
-    componentDidMount(){
-        getSatellites(this.props.id_da_lua).then(data => {
-            this.setState(state => ({
-                satellites: data['satellites']
-            }))
-        })
-    }
-    render() {
-        let title;
-    if (this.props.title_with_underline)
-        title = <h4><u>{this.props.name}</u></h4>
-    else
-        title = <h4>{this.props.name}</h4>
-    return (
-        <div>
-            {title}
-            <DescriptionWithLink link={this.props.link} text={this.props.text}/>
-            <GrayImg img_url={this.props.img_url} gray={this.props.gray_img} />
-            <h4>Satélites</h4>
-            <ul>
-                {this.state.satellites.map((satellite, index) => <li key={index}>{satellite.name}</li>)}
-            </ul>
-            <hr/>
-        </div>
-    )
-    }
-}
+const Planet = (props) => {
+  const [satellites, setSatellites] = useState([]);
+
+  useEffect(() => {
+    getSatellites(props.id_da_lua).then((data) => {
+      setSatellites(data["satellites"]);
+    });
+  }, []);
+
+  let title;
+  if (props.title_with_underline)
+    title = (
+      <h4>
+        <u>{props.name}</u>
+      </h4>
+    );
+  else title = <h4>{props.name}</h4>;
+  return (
+    <div>
+      {title}
+      <DescriptionWithLink link={props.link} text={props.text} />
+      <GrayImg img_url={props.img_url} gray={props.gray_img} />
+      <h4>Satélites</h4>
+      <ul>
+        {satellites.map((satellite, index) => (
+          <li key={index}>{satellite.name}</li>
+        ))}
+      </ul>
+      <hr />
+    </div>
+  );
+};
 export default Planet;
