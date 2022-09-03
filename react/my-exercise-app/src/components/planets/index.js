93c6508d53af06ba9e1baf6f344d8a59f3ae66dd
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-09-03 15:16:10 -0300

    Components with porps

--- react/my-exercise-app/src/components/planets/index.js
+++ react/my-exercise-app/src/components/planets/index.js
@@ -5,13 +5,9 @@ const Planets = () => {
         <>
             <h3>Planet List</h3>
             <hr/>
-            <Planet/>
-            <hr/>
-            <Planet/>
-            <hr/>
-            <Planet/>
-            <hr/>
-            <Planet/>
+            <Planet name="Mercúrio" description="Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. Mercúrio completa três rotações em torno de seu eixo a cada duas órbitas. O periélio da órbita de Mercúrio apresenta uma precessão de 5 600 segundos de arco por século, um fenômeno completamente explicado apenas a partir do século XX pela Teoria da Relatividade Geral formulada por Albert Einstein." img_url="https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/e8a8426182d5c9d3ce5d4d6e6358b128.jpg" />
+            <Planet name="Plutão" description="Plutão é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. Mercúrio completa três rotações em torno de seu eixo a cada duas órbitas. O periélio da órbita de Mercúrio apresenta uma precessão de 5 600 segundos de arco por século, um fenômeno completamente explicado apenas a partir do século XX pela Teoria da Relatividade Geral formulada por Albert Einstein." img_url="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/sonda-new-horizons-registrou-imagem-plutao-em-julho-2015-55a55da01dda4.jpg" />
+
         </>
     )
 }
---
+++ react/my-exercise-app/src/components/shared/img/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+const GrayImg = (props) => {
+    return (
+        <img class="gray-img" src={props.img_url} alt="Mercúrio"></img>
+    )
+}
+
+export default GrayImg;
