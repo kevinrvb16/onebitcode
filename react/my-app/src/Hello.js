01c45d5a228ff7d8228ddfe7c97c9d723f9e2260
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-09-03 11:59:35 -0300

    Alterado para fazer hello world

---
+++ react/my-app/src/Hello.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <h1>
+                Hello World!
+            </h1>
+        );
+    }
+}
+
+
+export default HelloWorld;
